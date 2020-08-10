278aa7aff05e161bb30828fbcc2dc6847def3948
marsha <marshaannon@gmail.com>
2020-08-09 23:10:16 -0400

    add colorize color (yellow & maagenta to strings

---
+++ lib/cli.rb
@@ -0,0 +1,52 @@
+require "pry"
+
+class CLI
+
+    def call
+        Scraper.scrape_TheActiveTimes
+        puts "\n \n  Hi, Welcome to The Best Botanical Gardens in America!".yellow.bold
+        puts ""
+        puts "  Which botanical garden would you like to learn more about?".yellow
+        puts ""
+        list_gardens
+        puts ""
+        pick_garden 
+    end
+
+    def list_gardens
+        Gardens.all.each.with_index(1) do | garden, i |
+        puts "  #{i}. #{garden.title}"
+        end
+    end
+
+    def pick_garden
+        puts ""
+        puts "  Please choose a number for more info on a Botanical Garden:".yellow
+        input = gets.chomp
+        if !input.to_i.between?(1, Gardens.all.count)
+            list_gardens
+            puts ""
+            puts "  Sorry, garden not found, please select another from the list".red.bold
+            puts ""
+            pick_garden   
+        else  garden = Gardens.all[input.to_i-1]
+            puts ""
+            puts "------------------------------ #{garden.title} ------------------------------".magenta
+            puts ""
+            puts "#{garden.body}"
+            puts ""
+            puts "  Would you like to check out another garden?".yellow
+            puts ""
+            puts "  Please type Y or N".yellow
+            another_garden = gets.strip.downcase
+        end
+
+        if another_garden == "y"
+            list_gardens
+            pick_garden
+        else input == "n"
+            puts "  When nature smiles, we all smile :) I hope you found what your looking for!".magenta
+            exit
+        end
+    end
+end
---
+++ lib/gardens.rb
@@ -0,0 +1,20 @@
+require "pry"
+
+class Gardens
+
+    attr_accessor :title, :body
+
+    @@all = []
+
+    def initialize(title, body)
+        @title = title
+        @body = body
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+    
+end
+
